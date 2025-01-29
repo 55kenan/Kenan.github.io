64e5d3a91553668c89074f3f1929cbb3ad671243
de-kenan <kenansametroll@gmail.com>
2025-01-29 14:25:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,84 @@ document.getElementById('convert').addEventListener('click', function() {
 });
 
 function convertToASCII(imageData) {
-    const chars = '!'^'^+%&/())=?_QXTIIOKT '; // ASCII karakter seti
+    const chars = '# Türkçe Harfler
+A: \u0041
+B: \u0042
+C: \u0043
+Ç: \u00C7
+D: \u0044
+E: \u0045
+F: \u0046
+G: \u0047
+Ğ: \u011E
+H: \u0048
+I: \u0049
+İ: \u0130
+J: \u004A
+K: \u004B
+L: \u004C
+M: \u004D
+N: \u004E
+O: \u004F
+Ö: \u00D6
+P: \u0050
+R: \u0052
+S: \u0053
+Ş: \u015E
+T: \u0054
+U: \u0055
+Ü: \u00DC
+V: \u0056
+Y: \u0059
+Z: \u005A
+
+# Sayılar
+0: \u0030
+1: \u0031
+2: \u0032
+3: \u0033
+4: \u0034
+5: \u0035
+6: \u0036
+7: \u0037
+8: \u0038
+9: \u0039
+
+# Özel Karakterler
+Boşluk: \u0020
+!: \u0021
+": \u0022
+#: \u0023
+$: \u0024
+%: \u0025
+&: \u0026
+': \u0027
+(: \u0028
+): \u0029
+*: \u002A
++: \u002B
+,: \u002C
+-: \u002D
+.: \u002E
+/: \u002F
+:: \u003A
+;: \u003B
+<: \u003C
+=: \u003D
+>: \u003E
+?: \u003F
+@: \u0040
+[: \u005B
+\: \u005C
+]: \u005D
+^: \u005E
+_: \u005F
+`: \u0060
+{: \u007B
+|: \u007C
+}: \u007D
+~: \u007E
+ '; // ASCII karakter seti
     const width = imageData.width;
     const height = imageData.height;
     let ascii = '';
